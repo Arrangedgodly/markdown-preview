47ae17e41153bdcb5339093c69918a381a541d10
Arrangedgodly <90996993+Arrangedgodly@users.noreply.github.com>
2023-06-10 17:16:06 -0600

    refactor: testing to see if this is enough to get through FCC requirements or if i need to troubleshoot more

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import {marked} from "marked";
+import DOMPurify from "dompurify";
+
+const defaultMarkdown = [
+  "# Welcome to my React Markdown Previewer!",
+  "## This is a sub-heading...",
+  "### And here's some other cool stuff:",
+  "Heres some code, `<div></div>`, between 2 backticks.",
+  "```",
+  "// this is multi-line code:",
+  "function anotherExample(firstLine, lastLine) {",
+  "  if (firstLine == '```' && lastLine == '```') {",
+  "    return multiLineCode;",
+  "  }",
+  "}",
+  "```",
+  "You can also make text **bold**... whoa!",
+  "Or _italic_.",
+  "Or... wait for it... **_both!_**",
+  "And feel free to go crazy ~~crossing stuff out~~.",
+  "There's also [links](https://www.freecodecamp.org), and",
+  "> Block Quotes!",
+  "And if you want to get really crazy, even tables:",
+  "Wild Header | Crazy Header | Another Header?",
+  "------------ | ------------- | -------------",
+  "Your content can | be here, and it | can be here....",
+  "And here. | Okay. | I think we get it.",
+  "- And of course there are lists.",
+  "  - Some are bulleted.",
+  "     - With different indentation levels.",
+  "        - That look like this.",
+  "1. And there are numberered lists too.",
+  "1. Use just 1s if you want!",
+  "1. But the list goes on...",
+  "- Even if you use dashes or asterisks.",
+  "* And last but not least, let's not forget embedded images:",
+  "",
+  "![React Logo w/ Text](https://goo.gl/Umyytc)",
+]
+
+function App() {
+  const [markdown, setMarkdown] = useState("");
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    const cleanHtml = DOMPurify.sanitize(marked(markdown));
+    setHtml(cleanHtml);
+  }, [markdown]);
+
+  useEffect(() => {
+    const defaultMarkdownString = defaultMarkdown.join("\n");
+    setMarkdown(defaultMarkdownString);
+  }, []);
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setMarkdown(e.target.value);
+  };
+
+  return (
+    <main className="main">
+      <textarea
+        className="input"
+        id="editor"
+        value={markdown}
+        onChange={handleInputChange}
+      />
+      <div
+        className="preview"
+        id="preview"
+        dangerouslySetInnerHTML={{ __html: html }}
+      ></div>
+    </main>
+  );
+}
+
+export default App;
